27baf1c965141402fb0e39351741459685c616b9
nhrmywork <nhrmywork@example.com>
2025-09-01 16:55:02 +0530

    Added vercel insight package for my insights of page visotors

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './styles/globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+    <Analytics />
+  </>
+  // </StrictMode>,
 )
